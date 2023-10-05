40a7109c4ec43c461afd0716d4b1db7b3182e09f
Qasim_gitHub <qasimab222@gmail.com>
2023-10-05 17:52:16 +0500

    edit and delete Api

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -21,3 +21,28 @@ exports.addItemController = async (req, res) => {
     console.log(`error while call the addItemController api ${error}`);
   }
 };
+
+////edit Items
+exports.editItemController = async (req, res) => {
+  try {
+    await itemModal.findOneAndUpdate({ _id: req.body.itemId }, req.body, {
+      new: true,
+    });
+    res.status(201).send("Item Updated Successfully");
+  } catch (error) {
+    res.status(400).send("error", error);
+    console.log(`error while call the editItemController api ${error}`);
+  }
+};
+
+/////Delete items
+exports.deleteItemController = async (req, res) => {
+  try {
+    const { itemId } = req.body;
+    await itemModal.findOneAndDelete({ _id: itemId });
+    res.status(200).json("item Deleted");
+  } catch (error) {
+    res.status(400).send("error", error);
+    console.log("error while calling the editItemController Api", error);
+  }
+};
